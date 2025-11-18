4ff4be298ad16c161cb9c0b61fecc6cdc31eb314
Gabriel Soares <gabrielsoares@ba:14:15:74:4f:36.lan>
2025-11-18 09:45:02 +0000

    adiciona scripts `lint:check` e `lint:fix`

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # clone-tabnews
+
 Clone do TabNews criado para o curso.dev!
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-    return <h1>Te amo Helena meu amor! Linda da minha vida!!</h1>;
+  return <h1>Te amo Helena meu amor! Linda da minha vida!!</h1>;
 }
 
 export default Home;
