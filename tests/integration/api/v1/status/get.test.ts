c9a827a73f9ce1635ad4b4fd0be195eba7419bb0
Gabriel Soares <gabrielgyns@gmail.com>
2026-09-22 17:20:45 +0100

    tsconfig, db e tests

--- tests/integration/api/v1/status/get.test.ts
+++ tests/integration/api/v1/status/get.test.ts
@@ -1,11 +1,12 @@
-import database from "infra/databse";
+// import database from "infra/database";
 
 test("GET to /api/v1/status should return 200", async () => {
-    const result = await database.query("SELECT 1 + 1 as sum");
+  // const result = await database.query("SELECT 1 + 1 as sum");
+  // console.log(result.rows);
 
-    console.log(result.rows);
+  const response = await fetch("http://localhost:3000/api/v1/status");
 
-    const response = await fetch("http://localhost:3000/api/v1/status");
+  console.log(await response.json());
 
-    expect(response.status).toBe(200);
+  expect(response.status).toBe(200);
 });
